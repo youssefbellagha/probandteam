ae88f46259672ab79626a38ed4dc207099f9d11b
MontaserSaleh91 <montaser.alfrookh@gmail.com>
2020-03-06 02:29:39 +0200

    first commit

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,51 @@
+<!-- jQuery 3 -->
+    {{--<!-- jQuery 3 -->--}}
+    <script src="{{ asset('dashboard_files/js/jquery.min.js') }}"></script>
+
+    {{--noty--}}
+    <link rel="stylesheet" href="{{ asset('dashboard_files/plugins/noty/noty.css') }}">
+    <script src="{{ asset('dashboard_files/plugins/noty/noty.min.js') }}"></script>
+
+    {{--morris--}}
+    <link rel="stylesheet" href="{{ asset('dashboard_files/plugins/morris/morris.css') }}">
+
+    {{--<!-- iCheck -->--}}
+    <link rel="stylesheet" href="{{ asset('dashboard_files/plugins/icheck/all.css') }}">
+
+    {{--html in  ie--}}
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    {{--<!-- Bootstrap 3.3.7 -->--}}
+<script src="{{ asset('dashboard_files/js/bootstrap.min.js') }}"></script>
+
+{{--icheck--}}
+<script src="{{ asset('dashboard_files/plugins/icheck/icheck.min.js') }}"></script>
+
+{{--<!-- FastClick -->--}}
+<script src="{{ asset('dashboard_files/js/fastclick.js') }}"></script>
+
+{{--<!-- AdminLTE App -->--}}
+<script src="{{ asset('dashboard_files/js/adminlte.min.js') }}"></script>
+
+{{--ckeditor standard--}}
+<script src="{{ asset('dashboard_files/plugins/ckeditor/ckeditor.js') }}"></script>
+
+{{--jquery number--}}
+<script src="{{ asset('dashboard_files/js/jquery.number.min.js') }}"></script>
+
+{{--print this--}}
+<script src="{{ asset('dashboard_files/js/printThis.js') }}"></script>
+
+{{--morris --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="{{ asset('dashboard_files/plugins/morris/morris.min.js') }}"></script>
+
+{{--custom js--}}
+<script src="{{ asset('dashboard_files/js/custom/image_preview.js') }}"></script>
+<script src="{{ asset('dashboard_files/js/custom/order.js') }}"></script>
+
+<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
